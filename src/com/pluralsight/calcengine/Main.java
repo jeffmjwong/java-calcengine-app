bcc363ef58f0eea6b30fa4bf16a00a182b372f75
Jeff Wong <v-jwong1@expedia.com>
2019-12-09 19:55:42 +1000

    initial commit

--- src/com/pluralsight/calcengine/Main.java
+++ src/com/pluralsight/calcengine/Main.java
@@ -3,9 +3,23 @@ package com.pluralsight.calcengine;
 public class Main {
 
     public static void main(String[] args) {
-        double val1;
-        double val2;
+        double val1 = 100.0d;
+        double val2 = 50.0d;
         double result;
-        char opCode;
+        char opCode = 'd';
+
+        if (opCode == 'a') {
+            result = val1 + val2;
+        } else if (opCode == 's') {
+            result = val1 - val2;
+        } else if (opCode == 'd') {
+            result = val1 / val2;
+        } else if (opCode == 'm') {
+            result = val1 * val2;
+        } else {
+            result = 0.0d;
+        }
+
+        System.out.println(result);
     }
 }
