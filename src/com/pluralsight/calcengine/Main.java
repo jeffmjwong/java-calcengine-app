a521cff52b0c5db5d8b8d02e23b3b4c600e44583
Jeff Wong <v-jwong1@expedia.com>
2019-12-10 17:25:41 +1000

    multiple op codes and simple calculations

--- src/com/pluralsight/calcengine/Main.java
+++ src/com/pluralsight/calcengine/Main.java
@@ -4,23 +4,24 @@ public class Main {
 
     public static void main(String[] args) {
         double val1 = 100.0d;
-        double val2 = 0.0d;
-        double result;
-        char opCode = 'd';
+        double val2 = 25.0d;
+        char opCode = 'm';
 
+        System.out.println(calculate(val1, val2, opCode));
+    }
+
+    private static double calculate(double val1, double val2, char opCode) {
         if (opCode == 'a') {
-            result = val1 + val2;
+            return val1 + val2;
         } else if (opCode == 's') {
-            result = val1 - val2;
+            return val1 - val2;
         } else if (opCode == 'd') {
-            result = val2 == 0.0d ? 0.0d : val1 / val2;
+            return val2 == 0.0d ? 0.0d : val1 / val2;
         } else if (opCode == 'm') {
-            result = val1 * val2;
+            return val1 * val2;
         } else {
             System.out.println("Error - invalid opCode!");
-            result = 0.0d;
+            return 0.0d;
         }
-
-        System.out.println(result);
     }
 }
