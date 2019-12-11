660ea04e98220901a48d0f098abaf13dd7f19959
Jeff Wong <v-jwong1@expedia.com>
2019-12-11 19:44:44 +1000

    create new create method to create MathEquation class instances

--- src/com/pluralsight/calcengine/Main.java
+++ src/com/pluralsight/calcengine/Main.java
@@ -4,30 +4,18 @@ public class Main {
 
     public static void main(String[] args) {
         MathEquation[] equations = new MathEquation[4];
-        equations[0] = new MathEquation(100.0d, 50.0d, 'd');
-        equations[1] = new MathEquation(25.0d, 92.0d, 'a');
-        equations[2] = new MathEquation(225.0d, 17.0d, 's');
-        equations[3] = new MathEquation(11.0d, 3.0d, 'm');
+        equations[0] = create(100.0d, 50.0d, 'd');
+        equations[1] = create(25.0d, 92.0d, 'a');
+        equations[2] = create(225.0d, 17.0d, 's');
+        equations[3] = create(11.0d, 3.0d, 'm');
 
-        for (int i = 0; i < equations.length; i++) {
-            equations[i].execute();
-            System.out.println(equations[i].result);
+        for (MathEquation equation : equations) {
+            equation.execute();
+            System.out.println(equation.result);
         }
     }
 
-    private static double calculate(double leftVal, double rightVal, char opCode) {
-        switch (opCode) {
-            case 'a':
-                return leftVal + rightVal;
-            case 's':
-                return leftVal - rightVal;
-            case 'd':
-                return rightVal == 0.0d ? 0.0d : leftVal / rightVal;
-            case 'm':
-                return leftVal * rightVal;
-            default:
-                System.out.println("Error - invalid opCode!");
-                return 0.0d;
-        }
+    private static MathEquation create(double leftVal, double rightVal, char opCode) {
+        return new MathEquation(leftVal, rightVal, opCode);
     }
 }
