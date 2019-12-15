336fb81427783199f4e983db8e12083b96a9905a
Jeff Wong <v-jwong1@expedia.com>
2019-12-15 19:46:45 +1000

    create basic methods for CalculateBase class

--- src/com/pluralsight/calcengine/CalculateBase.java
+++ src/com/pluralsight/calcengine/CalculateBase.java
@@ -1,4 +1,28 @@
 package com.pluralsight.calcengine;
 
 public class CalculateBase {
+    private double leftVal;
+    private double rightVal;
+    private double result;
+
+    public double getLeftVal() {
+        return this.leftVal;
+    }
+    public void setLeftVal(double leftVal) {
+        this.leftVal = leftVal;
+    }
+    public double getRightVal() { return this.rightVal;}
+    public void setRightVal(double rightVal) {
+        this.rightVal = rightVal;
+    }
+    public double getResult() {
+        return this.result;
+    }
+
+    public CalculateBase() {}
+
+    public CalculateBase(double leftVal, double rightVal) {
+        this.leftVal = leftVal;
+        this.rightVal = rightVal;
+    }
 }
