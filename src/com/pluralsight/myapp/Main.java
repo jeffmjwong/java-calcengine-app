c85545182472b60699bae2e70e2cb9a5edf8a2a3
Jeff Wong <v-jwong1@expedia.com>
2019-12-26 14:03:51 +1000

    create PowerOf class

--- src/com/pluralsight/myapp/Main.java
+++ src/com/pluralsight/myapp/Main.java
@@ -21,6 +21,7 @@ public class Main {
 
         String[] statements = {
                 "add 25.0 92.0", // 25.0 + 92.0 = 117.0
+                "power 5.0 2.0", // 5.0 ^ 2.0 = 25.0
         };
 
         MathProcessing[] handlers = {
