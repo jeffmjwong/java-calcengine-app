1bbc33487b4ea94573dbd897ba4433fb9266a429
Jeff Wong <v-jwong1@expedia.com>
2019-12-26 13:19:36 +1000

    specify implementation details for MathProcessing interface

--- src/com/pluralsight/calcengine/MathProcessing.java
+++ src/com/pluralsight/calcengine/MathProcessing.java
@@ -1,4 +1,8 @@
 package com.pluralsight.calcengine;
 
 public interface MathProcessing {
+    String SEPARATOR = " ";
+    String getKeyword(); // e.g. add
+    char getSymbol(); // e.g. +
+    double doCalculation(double leftVal, double rightVal);
 }
